8869d9e29e2c76dcdfc47a0832173512adc5d715
adbraguta <adelia.braguta@endava.com>
2023-10-16 16:11:28 +0300

    Add timer, start-restart button etc

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ function createBoard(boardSize) {
     return board
 }
 
-let boardSize = 10
-const board = createBoard(boardSize)
-generateMines(board, boardSize)
 
 function generateMines(board, boardSize) {
     let numberMines = 20;
@@ -35,33 +32,192 @@ function generateMines(board, boardSize) {
     }
 }
 
-console.log(board)
+let countFlags = 0;
+
+function display(board) {
+    tiles = displayBoard(board)
+    tiles.forEach(tile => {
+        document.querySelector('.start-board').appendChild(tile)
+        tile.classList.add('start-tile')
+    })
+}
+
+function displayEmpty(board, element, x, y) {
+    if (
+        element.classList.contains('revealed') ||
+        element.classList.contains('flag'))
+    {
+        return;
+    }
+    element.classList.add('revealed', 'empty');
+    for (let i = x - 1; i <= x + 1; i++) {
+        for (let j = y - 1; j <= y + 1; j++) {
+            if (i >= 0 && i < boardSize && j >= 0 && j < boardSize) {
+                const adjacentElement = tiles[i * boardSize +j];
+                if (board[i][j] === 0) {
+                    displayEmpty(board, adjacentElement, i, j);
+                }
+                if (board[i][j] > 0) {
+                    adjacentElement.classList.add('revealed', 'value' + board[i][j]);
+                    adjacentElement.innerHTML = board[i][j];
+                }
+            }
+        }
+    }
+}
+
 
 function displayBoard(board) {
     let elements = [];
-    board.forEach(row => {
-        row.forEach(value => {
+    board.forEach((row, x) => {
+        row.forEach((tile, y) => {
             let element = document.createElement('div')
             element.classList.add('tile')
-            if (value < 0) {
-                element.classList.add('mine')
-            }
-            if (value > 0) {
-                element.innerHTML = value
-                element.classList.add('value')
-            }
+            element.addEventListener('click', () => {
+                if (element.classList.contains('flag')) {
+                    return
+                }
+                if (board[x][y] === 0) {
+                    displayEmpty(board, element, x, y)
+                }
+                if (board[x][y] < 0) {
+                    GameOver(board)
+                }
+                if (board[x][y] > 0) {
+                    element.innerHTML = board[x][y]
+                    element.classList.add('revealed', 'value' + board[x][y]);
+                }
+                if(WinGame()){
+                    gameOver.innerHTML = "You Won!"
+                    gameOver.style.color = "#8EEDD1"
+                    gameOver.style.visibility = "visible"
+                }
+            })
+            element.addEventListener('contextmenu', (e) => {
+                e.preventDefault()
+                if(element.classList.contains('revealed')){
+                    return
+                }
+                if(!element.classList.contains('flag')){
+                    element.innerHTML = '🚩'
+                    element.classList.add('flag')
+                    countFlags++
+                } else if(element.classList.contains('flag')){
+                    element.innerHTML = ' '
+                    element.classList.remove('flag')
+                    countFlags--                }
+                const flags = document.getElementById('count-flags')
+                flags.innerText = countFlags;
+            })
             elements.push(element)
         })
     })
     return elements
 }
 
-countMines(board);
-const tiles = displayBoard(board)
-tiles.forEach(tile => {
-    document.querySelector('.board').appendChild(tile)
+let boardSize = 10
+let tiles
+const startRestartButton = document.getElementById("st");
+let gameStarted = false;
 
-})
+const startBoard = createBoard(boardSize)
+display(startBoard)
+
+startRestartButton.addEventListener("click", function () {
+    if (!gameStarted) {
+        StartGame();
+        startRestartButton.innerText = "Restart Game";
+    } else {
+        RestartGame();
+    }
+    gameStarted = true;
+});
+
+
+function StartGame() {
+    StartTimer()
+    startRestartButton.style.visibility = "hidden"
+    const board = createBoard(boardSize)
+    console.log(board)
+    generateMines(board, boardSize)
+    countMines(board);
+    const display = document.querySelector(".board")
+    display.style.display = "grid"
+    tiles = displayBoard(board)
+    tiles.forEach(tile => {
+        document.querySelector('.board').appendChild(tile)
+    })
+    const startBoard = document.querySelector(".start-board")
+    startBoard.style.display = "none"
+    const flags = document.getElementById('count-flags')
+    flags.innerText = 0;
+    countFlags = 0
+
+
+}
+
+const gameOver = document.getElementById("game-over")
+
+function RestartGame() {
+    const boardElement = document.querySelector('.board');
+    while (boardElement.firstChild) {
+        boardElement.removeChild(boardElement.firstChild);
+    }
+    StartTimer()
+    StartGame()
+    startRestartButton.innerText = "Start Game";
+    gameOver.style.visibility = "hidden"
+    gameStarted = false;
+}
+
+function GameOver(board) {
+    clearInterval(timeInterval);
+    board.forEach((row, x) => {
+        row.forEach((value, y) => {
+            if (value === -1) {
+                const element = tiles[x * boardSize + y];
+                element.innerHTML = '💣';
+                element.classList.add('mine');
+                gameOver.style.visibility = "visible"
+                gameOver.style.color = "#AB091E"
+                startRestartButton.style.visibility = "visible"
+            }
+        })
+    })
+}
+
+function WinGame() {
+    let counter = 0
+    for(let x = 0; x<boardSize; x++){
+        for(let y =0; y< boardSize; y++){
+            if(tiles[x *boardSize+ y].classList.contains('revealed')){
+                counter++
+            }
+        }
+    }
+    if(counter === 80){
+        return true;
+    }
+}
+
+const timer = document.getElementById("timer")
+let timeInterval
+
+function StartTimer() {
+    clearInterval(timeInterval)
+    let seconds = 0,
+        minutes = 0
+
+    timeInterval = setInterval(() => {
+        timer.innerHTML =
+            (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+        seconds++
+        if (seconds === 60) {
+            minutes++
+            seconds = 0
+        }
+    }, 1000)
+}
 
 function countMines(board) {
     let counter = 0;
@@ -122,6 +278,3 @@ function countMines(board) {
         }
     }
 }
-
-
-
